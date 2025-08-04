a8eaeb25d7268254a5e9995e780ea84f23f36aac
SACPA <williamjoel1909@gmail.com>
2025-08-04 15:47:49 -0500

    test8

--- back/server.js
+++ back/server.js
@@ -39,7 +39,7 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: DATABASE_URL,
 });
-console.log('DEBUG: Firebase Admin SDK inicializado para Realtime databeis.');
+console.log('DEBUG: Firebase Admin SDK inicializado para Realtime databei.');
 // ------------------------------------------
 
 // --- CONFIGURACIÓN DE reCAPTCHA ---
